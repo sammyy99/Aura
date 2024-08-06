805b3b8a97f9cd1387cea3e42ade2d7b8c450718
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-08-07 04:38:53 +0530

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className = {`flex-1 items-center justify-center bg-red-600`}>
+      <Text className = {`text-2xl`}>Aura</Text>
+    </View>
+  );
+}
