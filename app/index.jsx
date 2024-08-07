38e30b39af3c4388cc4ce578e353edbd0ddd0f14
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-08-07 19:41:10 +0530

    Structure defined

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className = {`flex-1 items-center justify-center bg-red-600`}>
-      <Text className = {`text-2xl`}>Aura</Text>
+      <Text className = {`text-3xl`}>Aura!</Text>
     </View>
   );
 }
